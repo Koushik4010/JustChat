2c305787ce065ead72843d510c5d2bb613b63607
Koushik4010 <98331437+Koushik4010@users.noreply.github.com>
2023-10-22 10:43:50 +0530

    Add files via upload

---
+++ front_end/src/components/Chatbox.js
@@ -0,0 +1,26 @@
+import { Box } from "@chakra-ui/react";
+import "./styles.css";
+import SingleChat from "./SingleChat";
+import { ChatState } from "../Context/ChatProvider";
+
+const Chatbox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = ChatState();
+
+  return (
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={1}
+      m={-2}
+      bg="#E8E8E8"
+      w={{ base: "100%", md: "73%" }}
+      // borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
+};
+
+export default Chatbox;
---
+++ front_end/src/components/miscellaneous/ProfileModal.js
@@ -0,0 +1,75 @@
+import { InfoIcon } from "@chakra-ui/icons";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Text,
+  
+} from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
+
+const ProfileModal = ({ user, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton
+          d={{ base: "flex" }}
+          _hover={{ backgroundColor: "white", color: "black" }}
+          icon={<InfoIcon />}
+          onClick={onOpen}
+        />
+      )}
+      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent h="410px">
+          <ModalHeader
+            fontSize="40px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            {user.name}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Avatar
+                size="sm"
+                cursor="pointer"
+                size="2xl"
+                name={user.name}
+                src={user.pic}
+            />
+            
+            <Text
+              fontSize={{ base: "20px", md: "25px" }}
+              fontFamily="Work sans"
+            >
+              Email: {user.email}
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfileModal;
